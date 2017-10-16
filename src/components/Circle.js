e91c22ad42b3456d338be0b1d14159a02f674c81
Zeynep KARATATAR <zkaratatar@innova.com.tr>
2017-10-16 11:19:04 +0300

    moving circle

--- src/components/Circle.js
+++ src/components/Circle.js
@@ -2,29 +2,37 @@ import React, {Component} from 'react';
 
 class Circle extends Component{
 
-    circleStyle(size, position, color){
-        console.log(position);
-        let dim = size + 'px';
-        let style = {
-            width: dim,
-            height: dim,
-            backgroundColor: color,
-            top: position.top + 'px',
-            left: position.left + 'px',
-            position: 'absolute',
-            borderRadius:50,
+    constructor(props){
+        super(props);
+        this.state = {
+            style:{
+                top:0,
+                left:0,
+                width:20,
+                height:20,
+                borderRadius:50,
+                backgroundColor: '#df12ab',
+                position:'relative'
+            }
         }
-        return style;
     }
 
-    blowCircle(e){       
+    componentWillReceiveProps(){
+        let newStyle = Object.assign({}, this.state.style);
+        newStyle.top += 10;
+        this.setState({
+            style:newStyle
+        });
+       
+    }
+
+    blow(e){      
         e.target.style.visibility = 'hidden';
     }
-   
+
+
     render(){
-        return(
-             <div style={this.circleStyle} onClick={this.blowCircle.bind(this)}></div>
-        );       
+        return( <div style={this.state.style} onClick={this.blow.bind(this)}> </div>);       
     }
 
 }
